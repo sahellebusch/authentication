bc65893553d7d74559fa9d9c508049072d22834e
Sean Hellebusch <sean.hellebusch@hingehealth.com>
2020-04-27 17:37:33 -0700

    7: Add Convey testing lib

--- controllers/status_test.go
+++ controllers/status_test.go
@@ -7,24 +7,26 @@ import (
 	"testing"
 
 	"github.com/jinzhu/gorm"
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStatusRoute(t *testing.T) {
+	Convey("When the server is running", t, func() {
+		db, err := gorm.Open("sqlite3", "./test.db")
+		if err != nil {
+			panic("Failed to connect to database!")
+		}
 
-	db, err := gorm.Open("sqlite3", "./test.db")
-	if err != nil {
-		panic("Failed to connect to database!")
-	}
+		defer db.Close()
 
-	defer db.Close()
+		router := router.SetupRouter(db)
 
-	router := router.SetupRouter(db)
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/status", nil)
+		router.ServeHTTP(w, req)
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/status", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "{\"status\":\"we good\"}", w.Body.String())
+		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, "{\"status\":\"we good\"}", w.Body.String())
+	})
 }
